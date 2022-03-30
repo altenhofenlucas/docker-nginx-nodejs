26367167a70801a860f76764bf54dfb3bd84deed
Lucas <lucas.a.bertrand@gmail.com>
2022-03-29 22:58:02 -0300

    Adding api to get names

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'mysql',
+    user: 'app',
+    password: 'app',
+    database: 'app'
+})
+
+const personName = "Lucas"
+connection.query(`INSERT INTO people(name) VALUES('${personName}');`)
+
+const app = express()
+
+app.get('/', (req, res) => {
+    connection.query('SELECT name FROM people;', (err, result, fields) => {
+        res.send(`<h1>Full Cycle Rocks!</h1><br><ul>${result.map(person => `<li>${person.name}</li>`)}</ul>`)
+    })    
+})
+
+app.listen(3000, () => console.log('Server is up!'))
